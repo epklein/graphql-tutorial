c7d8c0d3582882f28cf48b9dfe641e84e7cfd834
Eduardo Klein <eduardo.klein@vtex.com.br>
2023-03-28 20:39:10 -0300

    Basic structure with graphql schema

---
+++ README.md
@@ -0,0 +1,3 @@
+# GraphQL tutorial
+
+Following [this tutorial](https://www.youtube.com/playlist?list=PL4cUxeGkcC9iK6Qhn-QLcXCXPQUov1U7f) from [The Net Ninja](https://www.youtube.com/@NetNinja) on Youtube, to learn a bit about GraphQL in practice.
---
+++ server/schema/schema.js
@@ -0,0 +1,29 @@
+const graphql = require('graphql');
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString }
+    })
+})
+
+const RootQuery = new graphql.GraphQLInputObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: {id: { type: GraphQLString }},
+            resolve(parent, args) {
+                // code to get data from db / other source
+            }
+        }
+    }
+});
+
+module.exports = new graphql.GraphQLSchema({
+    query: RootQuery
+})
