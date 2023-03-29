416c7d64b5f45ee5048180cae4c94d9db5bdb0c2
Eduardo Klein <eduardo.klein@vtex.com.br>
2023-03-28 22:08:56 -0300

    Ability to query books by id with dummy data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,12 @@
 const graphql = require('graphql');
-
+const _ = require('lodash');
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// a dummy list to test
+var books = [
+    { name: 'How to Win Friends and Influence People', genre: 'Leadership', id: '1' },
+    { name: 'Accelerate: The Science of Lean Software and DevOps', genre: 'Software Engineering', id: '2' }
+];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -9,9 +15,9 @@ const BookType = new GraphQLObjectType({
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
     })
-})
+});
 
-const RootQuery = new graphql.GraphQLInputObjectType({
+const RootQuery = new graphql.GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
         book: {
@@ -19,6 +25,7 @@ const RootQuery = new graphql.GraphQLInputObjectType({
             args: {id: { type: GraphQLString }},
             resolve(parent, args) {
                 // code to get data from db / other source
+                return _.find(books, { id: args.id });
             }
         }
     }
